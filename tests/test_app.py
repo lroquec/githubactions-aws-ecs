e14e713cc1faaf1b5a7936a8738797ae648baa62
Laura Roque <lroquecu@gmail.com>
2024-11-08 12:16:29 +0100

    try

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,34 +1,16 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 import os
+from selenium.webdriver.common.by import By
 
 os.environ['OTEL_TRACES_EXPORTER'] = 'none'
 
 
-def test_app_loads_correctly():
+def test_app_loads_correctly(driver):
     APP_HOST = os.environ.get("APP_HOST", "app")
     APP_PORT = os.environ.get("APP_PORT", "5000")
 
     app_url = f"http://{APP_HOST}:{APP_PORT}"
     print(f"\nAttempting to access: {app_url}")
 
-    chrome_options = Options()
-    chrome_options.add_argument('--disable-features=ChromeWhatsNewUI')
-    chrome_options.add_argument('--disable-domain-reliability')
-    chrome_options.add_argument('--disable-logging')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--disable-web-security')
-    chrome_options.add_argument('--allow-running-insecure-content')
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--allow-insecure-localhost')
-    chrome_options.add_argument('--disable-features=Upgrade-Insecure-Requests')
-
-    driver = webdriver.Remote(
-        command_executor=os.environ['SELENIUM_REMOTE_URL'],
-        options=chrome_options
-    )
 
     try:
         print("Starting page get...")
@@ -41,7 +23,9 @@ def test_app_loads_correctly():
         print(f"Body found. Full text:\n{body.text}")
 
         print("Verifying 'very simple' text...")
-        assert "very simple" in body.text, f"'very simple' text not found. Current text: {body.text}"
+        assert (
+            "very simple" in body.text
+        ), f"'very simple' text not found. Current text: {body.text}"
         print("Test completed successfully")
 
     except Exception as e:
@@ -51,6 +35,3 @@ def test_app_loads_correctly():
         print("Page source:")
         print(driver.page_source)
         raise
-
-    finally:
-        driver.quit()
